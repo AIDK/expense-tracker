c37a3b46634b0396bf6b55edcdb05911d1635a4b
Andries de Kock <andries.de@gmail.com>
2023-04-25 09:51:23 +0200

    Integrated Hook Form and Zod

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,29 +1,64 @@
-import React from "react";
+import categories from "../categories";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  description: z.string().min(3).max(50),
+  amount: z.number().min(0.01).max(100_000),
+  category: z.enum(categories),
+});
+
+type ExpenseFormData = z.infer<typeof schema>;
 
 const ExpenseForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
         </label>
-        <input id="description" type="text" className="form-control" />
+        <input
+          {...register("description")}
+          id="description"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
-        <input id="amount" type="text" className="form-control" />
+        <input
+          {...register("amount")}
+          id="amount"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="category" className="form-label">
           Category
         </label>
-        <select id="category" className="form-select">
+        <select {...register("category")} id="category" className="form-select">
           <option value=""></option>
           {/* Map categories */}
+          {categories.map((category) => (
+            <option id={category} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </div>
+      <button className="btn btn-primary">Submit</button>
     </form>
   );
 };
