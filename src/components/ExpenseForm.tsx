f72272709db5f99ce06e5c11dec8befb53e8568c
Andries de Kock <andries.de@gmail.com>
2023-04-25 10:50:51 +0200

    Added schema validation using Zod

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,62 +1,97 @@
 import categories from "../categories";
-import { FieldValues, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+// validation schema (sets field character limits, sets field types and error messages when validation fails)
 const schema = z.object({
-  description: z.string().min(3).max(50),
-  amount: z.number().min(0.01).max(100_000),
-  category: z.enum(categories),
+  description: z
+    .string()
+    .min(3, { message: "Description should be at least 3 characters." })
+    .max(50),
+  amount: z
+    .number({ invalid_type_error: "Amount is required" })
+    .min(0.01)
+    .max(100_000),
+  category: z.enum(categories, {
+    errorMap: () => ({ message: "Category is required" }),
+  }),
 });
 
 type ExpenseFormData = z.infer<typeof schema>;
 
-const ExpenseForm = () => {
+interface Props {
+  onSubmit: (data: ExpenseFormData) => void;
+}
+
+const ExpenseForm = ({ onSubmit }: Props) => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
 
-  const onSubmit = (data: FieldValues) => console.log(data);
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form
+      onSubmit={handleSubmit((data) => {
+        onSubmit(data);
+        reset();
+      })}
+    >
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
         </label>
         <input
-          {...register("description")}
+          {...register("description")} // register `description` so that it can be validated
           id="description"
           type="text"
           className="form-control"
         />
+        {/* display error message when description fails validation 
+        TODO: Replace error message with validator error message */}
+        {errors.description && (
+          <p className="text-danger">
+            Description is required and must be at least 3 characters
+          </p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
         <input
-          {...register("amount")}
+          {...register("amount")} // register `amount` identifier so that it can be validated
           id="amount"
           type="text"
           className="form-control"
         />
+        {/* display error message when amount fails validation
+         TODO: Replace error message with validator error message */}
+        {errors.amount && <p className="text-danger">Amount is required</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="category" className="form-label">
           Category
         </label>
-        <select {...register("category")} id="category" className="form-select">
+        <select
+          {...register("category")} // register `category` so that it can be validated
+          id="category"
+          className="form-select"
+        >
           <option value=""></option>
-          {/* Map categories */}
           {categories.map((category) => (
             <option id={category} value={category}>
               {category}
             </option>
           ))}
         </select>
+        {/* display error message when category fails validation 
+        TODO: Replace error message with validator error message */}
+        {errors.category && (
+          <p className="text-danger"> Must select category</p>
+        )}
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
